0230ee2d595689d1982343682aee9ce636d6ea78
Dajan Radovic <dajo1986@gmail.com>
2020-04-12 18:07:16 +0200

    added month filer show all

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -1,9 +1,10 @@
 <template>
 <div>
-  <label for="exampleFormControlSelect1">Select month</label>
-    <select v-model="selectedMonth"  class="form-control form-control-sm" id="exampleFormControlSelect1">
-        <option disabled value="" selected>Pick a month</option>
-
+  <div class="row mt-3">
+  <div class="col-lg-6">
+     <select v-model="selectedMonth"  class="form-control form-control-sm" id="exampleFormControlSelect1">
+        <option disabled value="" selected>Filter by month</option>
+           
       <option>January</option>
       <option>Fabruary</option>
       <option>March</option>
@@ -16,9 +17,34 @@
       <option>October</option>
       <option>November</option>
       <option>Decembery</option>
+      <option>Show all</option>
+
       
     </select>
-<div class="flex-container mt-3">
+  </div>
+  <div class="col-lg-6">
+     <select   class="form-control form-control-sm" id="exampleFormControlSelect1">
+      <option disabled value="" selected >Filter by year</option>
+
+      <option>2020</option>
+      <option>2021</option>
+      <option>2022</option>
+      <option>2023</option>
+      <option>2024</option>
+      <option>2025</option>
+      <option>2026</option>
+      <option>2027</option>
+      <option>2028</option>
+      <option>2029</option>
+      <option>2030</option>
+      <option>Show all</option>
+
+           
+    </select>
+  </div>
+  </div>
+  <div class="flex-container mt-3">
+ 
 
   <div class="card border border-dark" style="width: 18rem;">
  <i class="fa fa-plus mt-2" style="color:green;"></i>
@@ -53,6 +79,7 @@ export default {
   data () {
     return {
       selectedMonth:'',
+      selectedYear:'',
     
 
 
@@ -71,57 +98,82 @@ export default {
   computed:{
      incometotal(){
 
-       if (this.selectedMonth==''){
-         let a=0;
-        this.incomesList.forEach(function(item){
-            a= a + parseInt(item.income);
+                    if (this.selectedMonth==''){
+                      let a=0;
+                      this.incomesList.forEach(function(item){
+                          a= a + parseInt(item.income);
 
-        });
+                      });
 
-        return a;
-       }
+                      return a;
+                    }
 
-       else{
+                    else if(this.selectedMonth=='Show all'){
 
-          let a=0;
-        this.incomesListByMonth.forEach(function(item){
-            a= a + parseInt(item.income);
+                        let a=0;
+                      this.incomesList.forEach(function(item){
+                          a= a + parseInt(item.income);
 
-        });
+                      });
 
-        return a;
+                      return a;
+
+
+                    }
+
+                    else{
+
+                        let a=0;
+                      this.incomesListByMonth.forEach(function(item){
+                          a= a + parseInt(item.income);
+
+                      });
+
+                      return a;
 
 
 
-       }
+                    }
 
      
        },
        expansetotal(){
 
-           if (this.selectedMonth==''){
-         let a=0;
-        this.expansesList.forEach(function(item){
-            a= a + parseInt(item.expanse);
+                      if (this.selectedMonth==''){
+                    let a=0;
+                    this.expansesList.forEach(function(item){
+                        a= a + parseInt(item.expanse);
 
-                              });
+                                          });
 
-                        return a;
-                                   }
+                                    return a;
+                                              }
 
-       else{
+                  else if(this.selectedMonth=='Show all'){
 
-          let a=0;
-        this.expansesListByMonth.forEach(function(item){
-            a= a + parseInt(item.expanse);
+                    let a=0;
+                    this.expansesList.forEach(function(item){
+                        a= a + parseInt(item.expanse);
 
-        });
+                    });
 
-        return a;
+                    return a;
+
+                  }
+                  
+                  else{
+
+                      let a=0;
+                    this.expansesListByMonth.forEach(function(item){
+                        a= a + parseInt(item.expanse);
+
+                    });
+
+                    return a;
 
 
 
-       }
+                  }
 
      },
 
@@ -143,16 +195,22 @@ export default {
 
     chooseExpanseList(){
 
-      if (this.selectedMonth==''){
+                if (this.selectedMonth==''){
 
-        return this.expansesList;
-      }
-      
-      else{
+                  return this.expansesList;
+                }
 
-        return this.expansesListByMonth;
-      }
-    },
+                else if (this.selectedMonth=='Show all'){
+
+                    return this.expansesList;
+
+                }
+                
+                else{
+
+                  return this.expansesListByMonth;
+                }
+              },
 
 
 
@@ -174,6 +232,11 @@ export default {
       if (this.selectedMonth==''){
 
         return this.incomesList;
+      }
+
+      else if(this.selectedMonth=="Show all"){
+           return this.incomesList;
+
       }
       
       else{
